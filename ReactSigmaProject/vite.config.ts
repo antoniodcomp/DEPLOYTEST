0cece5f75cb701e1561203cf038d81920aebca84
Antonio Francisco <academico.antonio09@gmail.com>
2024-07-02 10:50:10 -0300

    teste deploy 02

--- ReactSigmaProject/vite.config.ts
+++ ReactSigmaProject/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base:"/Projeto-React-Sigma-PIBIC/",
+  base:"/DEPLOYTEST/",
   plugins: [
     react(),
     VitePWA({
